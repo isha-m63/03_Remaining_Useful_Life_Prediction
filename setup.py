6f17b8a0d162003e626dce0517c401c9cdfd5f3e
isha_mahuvakar <ishamahuvakar1428@gmail.com>
2026-06-24 11:31:55 +0530

    --

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
 #This helps creating ML application as a package 
 
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-HYPHEN_E_DOT = '-e .'
-def get_requiremenets(file_path: str) -> List[str]:
-    """This function returns list of requirements"""
-    requirements = []
-    with open(file_path) as file_obj: 
-        requirements = file_obj.readlines()
-        requirements = [req.replace('\n', '') for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove('HYPEN_E_DOT')
-     
+setup(
+name='03_Remaining_Useful_Life_Prediction',
+version='0.0.1',
+author='Isha',
+author_email='ishamahuvakar1428@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
 
-
-setup = ( 
-    name = '03_Remaining_Useful_Life_Prediction', 
-    version = '0.0.1', 
-    author = 'Isha', 
-    packages = find_packages(), 
-    install_requires = get_requiremenets('requirements.txt')
 )
