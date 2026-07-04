ad9f7a3444e8aec3ddb3aa4fd6056982f30dc535
isha_mahuvakar <ishamahuvakar1428@gmail.com>
2026-07-04 22:53:29 +0530

    Conformal calibration complete

--- src/components/conformal_calibration.py
+++ src/components/conformal_calibration.py
@@ -14,6 +14,7 @@ Steps:
 import os
 import sys
 import pickle
+import numpy as np
 from dataclasses import dataclass, field
 
 import pandas as pd
@@ -24,4 +25,35 @@ from src.logger import logging
 @dataclass
 class ConformalCalibratorConfig:
     residuals_file_path: str = os.path.join('artifacts', 'residuals.pkl')
+    alpha: float = 0.1      #Gives 90% prediction intervals by default, can be changed at evaluation time
+
+
+class ConformalCalibrator:
+    def __init__(self):
+        self.config = ConformalCalibratorConfig()
+
+    def fit(self, model, X_cal: pd.DataFrame, y_cal: pd.Series):
+        try:
+            logging.info("Fitting conformal calibrator...")
+            y_hat_cal = model.predict(X_cal)
+            residuals = abs(y_cal - y_hat_cal)
+            with open(self.config.residuals_file_path, 'wb') as f:
+                pickle.dump(residuals, f)
+            logging.info("Conformal calibrator fitted and residuals saved.")
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def predict_interval(self, model, X_test: pd.DataFrame, alpha: float):
+        try:
+            logging.info("Predicting intervals using conformal calibrator")
+            with open(self.config.residuals_file_path, 'rb') as f:
+                residuals = pickle.load(f)
+            n = len(residuals)
+            q_hat = np.quantile(residuals, np.ceil((1 - alpha) * (n + 1)) / n)
+            y_hat_test = model.predict(X_test)
+            lower_bound = y_hat_test - q_hat
+            upper_bound = y_hat_test + q_hat
+            return lower_bound, upper_bound
+        except Exception as e:
+            raise CustomException(e, sys)
     
