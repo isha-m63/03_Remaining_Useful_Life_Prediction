89cc491067e9fdda1e8a49f0993a5ec4de18bff9
isha_mahuvakar <ishamahuvakar1428@gmail.com>
2026-07-04 23:23:08 +0530

    --

--- src/components/gp_training.py
+++ src/components/gp_training.py
@@ -1,1 +1,12 @@
 
+import os
+import sys
+import pandas as pd
+
+from dataclasses import dataclass
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+
+
+from src.utils import save_object, evaluate_model
+from src.logger import logging
+from src.exception import CustomException
