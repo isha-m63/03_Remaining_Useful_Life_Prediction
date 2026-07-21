b12cf347e2609876ad466ae747fcce02b3c0e5e3
isha_mahuvakar <ishamahuvakar1428@gmail.com>
2026-07-21 22:01:10 +0530

    Included GP hyperparameters

--- configs/data_constants_config.py
+++ configs/data_constants_config.py
@@ -3,9 +3,13 @@ import sys
 
 RUL_CAP = 125
 PCA_N_COMPONENETS = 10
-
 ALPHA = 0.1
 
 #Hyperparameters for XGBoost
 LEARNING_RATE = 0.1
 N_ESTIMATORS = 32
+
+#Hyperparameters for GP
+LENGTH_SCALES = 
+NU = 2.5
+DOWNSAMPLING_STEP = 5
